bb594da6228e980fce7211da67b928fdaa971cbe
sravan9295 <saisravan9295@gmail.com>
2022-01-23 05:24:28 +0000

    doubt

---
+++ src/components/HistoryItem/index.js
@@ -0,0 +1,33 @@
+import './index.css'
+
+const HistoryItem = props => {
+  const {details, deleteHistoryItem} = props
+  const {id, timeAccessed, logoUrl, title, domainUrl} = details
+
+  const onDelete = () => {
+    deleteHistoryItem(id)
+  }
+
+  return (
+    <li>
+      <div className="history-item">
+        <p>
+          {timeAccessed}
+          <span>{domainUrl}</span>
+        </p>
+        <div className="image-and-url">
+          <img className="domain-logo" src={logoUrl} alt="domain logo" />
+          <p>{title}</p>
+        </div>
+        <button className="delete-button" type="button" onClick={onDelete}>
+          <img
+            alt="delete"
+            src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
+          />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default HistoryItem
---
+++ src/components/HistoryList/index.js
@@ -0,0 +1,66 @@
+import {Component} from 'react'
+import HistoryItem from '../HistoryItem'
+import './index.css'
+
+class HistoryList extends Component {
+  state = {searchInput: '', historyItems: ''}
+
+  onSearchInputChange = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  deleteHistoryItem = id => {
+    const {historyItems} = this.state
+    const filteredHistoryItems = historyItems.filter(item => item.id !== id)
+    this.setState({historyItems: filteredHistoryItems})
+  }
+
+  render() {
+    const {initialHistoryList} = this.props
+    // const {timeAccessed, logoUrl, title, domainUrl} = initialHistoryList
+    const {searchInput} = this.state
+
+    const searchResult = initialHistoryList.filter(histroyItem =>
+      histroyItem.title.toLowerCase().includes(searchInput.toLowerCase()),
+    )
+
+    return (
+      <div className="app-container">
+        <div className="navbar">
+          <img
+            className="app-logo"
+            alt="app logo"
+            src="https://assets.ccbp.in/frontend/react-js/history-website-logo-img.png"
+          />
+          <div className="search-container">
+            <img
+              className="search-icon"
+              alt="search"
+              src="https://assets.ccbp.in/frontend/react-js/search-img.png"
+            />
+            <input
+              onChange={this.onSearchInputChange}
+              type="search"
+              value={searchInput}
+              placeholder="Search history"
+            />
+          </div>
+        </div>
+
+        <div>
+          <ul className="history-list">
+            {searchResult.map(item => (
+              <HistoryItem
+                key={item.id}
+                details={item}
+                deleteHistoryItem={this.deleteHistoryItem}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default HistoryList
